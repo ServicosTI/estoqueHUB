7d4ee45515b05f253b634eef04b3053220451caf
Heitor Santos <heitorg33@gmail.com>
2024-07-15 12:12:07 -0300

    Inclusão de novos horarios para execução da API

--- src/server.js
+++ src/server.js
@@ -15,13 +15,24 @@ cron.schedule("0 8 * * *", () => {
   requestFlowService();
 });
 
+cron.schedule("0 10 * * *", () => {
+  console.log("Executando tarefa às 8:00");
+  requestFlowService();
+});
+
 cron.schedule("0 12 * * *", () => {
   console.log("Executando tarefa às 12:00");
   requestFlowService();
 });
 
-cron.schedule("0 20 * * *", () => {
+cron.schedule("0 14 * * *", () => {
+  console.log("Executando tarefa às 8:00");
+  requestFlowService();
+});
+
+cron.schedule("0 16 * * *", () => {
   console.log("Executando tarefa às 20:00");
 });
+
 
 app.listen(port, () => console.log(`Servidor na porta: ${port}!`));
